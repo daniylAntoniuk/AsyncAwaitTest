f461a434adc1ff81bd9598f97aaf19b5bc79a7d3
Daniil <danik22122005@gmail.com>
2021-03-25 15:15:46 +0200

    init

---
+++ AsyncTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var mainThreadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Main finish main thread: " + mainThreadId);
+            await MethodNonWork();
+            await MethodWork();
+            //Work
+            await Task.Factory.StartNew(() => MethodNonWork());
+        }
+        static async Task MethodNonWork()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Thread.Sleep(3000);
+            Console.WriteLine("Method finish id:" + threadId);
+        }
+        static async Task MethodWork()
+        {
+            await Task.Run(() =>
+            {
+                var threadId = Thread.CurrentThread.ManagedThreadId;
+                Thread.Sleep(3000);
+                Console.WriteLine("Method finish id:" + threadId);
+            });
+        }
+    }
+}
